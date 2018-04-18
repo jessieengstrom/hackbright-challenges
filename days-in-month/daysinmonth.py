37d453bb9585788a98122f43094a45a2e28cb364
Jessie Engstrom <jessielengstrom@gmail.com>
2018-04-18 13:26:38 -0700

    Finished days-in-month.

---
+++ days-in-month/daysinmonth.py
@@ -0,0 +1,76 @@
+"""How many days are there in a month?
+
+Given a string with a month and a year (separated by a space),
+returns the number of days in that month.
+
+For example::
+
+    >>> for i in range(1, 13):
+    ...     date = str(i) + " 2016"
+    ...     print "%s has %s days." % (date, days_in_month(date))
+    1 2016 has 31 days.
+    2 2016 has 29 days.
+    3 2016 has 31 days.
+    4 2016 has 30 days.
+    5 2016 has 31 days.
+    6 2016 has 30 days.
+    7 2016 has 31 days.
+    8 2016 has 31 days.
+    9 2016 has 30 days.
+    10 2016 has 31 days.
+    11 2016 has 30 days.
+    12 2016 has 31 days.
+
+    >>> days_in_month("02 2015")
+    28
+"""
+
+
+def is_leap_year(year):
+    """Is this year a leap year?
+
+    Every 4 years is a leap year::
+
+        >>> is_leap_year(1904)
+        True
+
+    Except every hundred years::
+
+        >>> is_leap_year(1900)
+        False
+
+    Except-except every 400::
+
+        >>> is_leap_year(2000)
+        True
+    """
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+def days_in_month(date):
+    """How many days are there in a month?"""
+    date = date.split()
+
+    thirty_days = set([4, 6, 9, 11])
+    if int(date[0]) in thirty_days:
+        return 30
+    elif int(date[0]) == 2:
+        if is_leap_year(int(date[1])):
+            return 29
+        else:
+            return 28
+    else:
+        return 31
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. W00T!\n"
